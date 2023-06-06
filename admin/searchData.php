52eafce50f7128009f8ddaab67274d39d84ec5c1
Rick Allen Magbanua <ramnashor2@gmail.com>
2023-06-06 14:24:30 +0800

    Added search function

--- admin/searchData.php
+++ admin/searchData.php
@@ -9,7 +9,7 @@ include 'db_connect.php';
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Search | GNHS PTA Payment System - Admin</title>
+    <title>Search Info | GNHS PTA Payment System - Admin</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/Nunito.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css">
@@ -76,89 +76,106 @@ include 'db_connect.php';
                         </ul>
                     </div>
                 </nav>
-                <div class="container-fluid" style="max-width: 50%;">
-                    <span>
-                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin: auto; background: none; display: block; shape-rendering: auto;" width="100px" height="100px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
-                            <g transform="translate(50 50)">
-                                <g transform="scale(0.8)">
-                                    <g transform="translate(-50 -50)">
-                                        <g>
-                                            <animateTransform attributeName="transform" type="translate" repeatCount="indefinite" dur="1s" values="-20 -20;20 -20;0 20;-20 -20" keyTimes="0;0.33;0.66;1"></animateTransform>
-                                            <path fill="#5699d2" d="M44.19 26.158c-4.817 0-9.345 1.876-12.751 5.282c-3.406 3.406-5.282 7.934-5.282 12.751 c0 4.817 1.876 9.345 5.282 12.751c3.406 3.406 7.934 5.282 12.751 5.282s9.345-1.876 12.751-5.282 c3.406-3.406 5.282-7.934 5.282-12.751c0-4.817-1.876-9.345-5.282-12.751C53.536 28.033 49.007 26.158 44.19 26.158z"></path>
-                                            <path fill="#1d3f72" d="M78.712 72.492L67.593 61.373l-3.475-3.475c1.621-2.352 2.779-4.926 3.475-7.596c1.044-4.008 1.044-8.23 0-12.238 c-1.048-4.022-3.146-7.827-6.297-10.979C56.572 22.362 50.381 20 44.19 20C38 20 31.809 22.362 27.085 27.085 c-9.447 9.447-9.447 24.763 0 34.21C31.809 66.019 38 68.381 44.19 68.381c4.798 0 9.593-1.425 13.708-4.262l9.695 9.695 l4.899 4.899C73.351 79.571 74.476 80 75.602 80s2.251-0.429 3.11-1.288C80.429 76.994 80.429 74.209 78.712 72.492z M56.942 56.942 c-3.406 3.406-7.934 5.282-12.751 5.282s-9.345-1.876-12.751-5.282c-3.406-3.406-5.282-7.934-5.282-12.751 c0-4.817 1.876-9.345 5.282-12.751c3.406-3.406 7.934-5.282 12.751-5.282c4.817 0 9.345 1.876 12.751 5.282 c3.406 3.406 5.282 7.934 5.282 12.751C62.223 49.007 60.347 53.536 56.942 56.942z"></path>
-                                        </g>
-                                    </g>
-                                </g>
-                            </g>
-                        </svg>
-                    </span>
-                    <h4 class="card-title text-center">Looking for something?</h4><br />
-                    <form action="search.php" method="POST">
-                        <div class="input-group">
-                            <input type="text" class="form-control" placeholder="Search..." name="search">
-                            <span class="input-group-btn">
-                                <button class="btn btn-primary" type="submit" name="submit">Search</button>
-                            </span>
-                    </form>
-                </div>
-            </div>
-            <div class="container-fluid mt-3" id="searchresult" style="max-width: 85%;">
-                <div class="card shadow">
-                    <div class="card-body">
-                        <table class="table table-hover table-borderless my-0">
-                            <thead>
-                                <tr>
-                                    <th>LRN</th>
-                                    <th>First Name</th>
-                                    <th>Last Name</th>
-                                    <th>Grade Level</th>
-                                    <th>Address</th>
-                                    <th>E-Mail Address</th>
-                                </tr>
-                            </thead>
+                <?php
+                $data = $_GET['id'];
+                $getStudentData = mysqli_query($cxn, "SELECT * FROM student WHERE lrn_id='$data'") or die("Error in query: $getStudentData." . mysqli_error($cxn));
+
+                if ($getStudentData) {
+                    $sd = mysqli_fetch_assoc($getStudentData);
+                    $first_name = $sd['first_name'];
+                    $last_name = $sd['last_name'];
+                    $lrn = $sd['lrn_id'];
+                    $grade_level = $sd['grade_level'];
+                    $email = $sd['email'];
+                    $address = $sd['address'];
+                    $pg = $sd['pg_name'];
+                    $date = strtotime($sd['date_created']);
+                    $regdate = date("F d, Y; h:i A", $date);
+
+                ?>
+                    <div class="container-fluid" style="max-width: 85%;">
+                        <div class="card shadow">
+                            <div class="card-body">
+                                <h3 class="card-title mb-3">Student Information</h3>
+                                <div class="row">
+                                    <div class="col-sm-3">Learners' Reference Number</div>
+                                    <div class="col-sm-8"><?php echo $lrn; ?></div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-3">Student Name</div>
+                                    <div class="col-sm-8"><?php echo $first_name; ?> <?php echo $last_name; ?></div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-3">Grade Level</div>
+                                    <div class="col-sm-8"><?php echo $grade_level; ?></div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-3">Present Address</div>
+                                    <div class="col-sm-8"><?php echo $address; ?></div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-3">E-mail Address</div>
+                                    <div class="col-sm-8"><?php echo $email; ?></div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-3">Parent/Guardian Name</div>
+                                    <div class="col-sm-8"><?php echo $pg; ?></div>
+                                </div>
+
                             <?php
-                            if (isset($_POST['search'])) {
-                                $search = $_POST['search'];
-
-                                $search_query = mysqli_query($cxn, "SELECT * FROM student WHERE id LIKE '%$search%' or lrn_id LIKE '%$search%' or first_name LIKE '%$search%' or last_name LIKE '%$search%' or email LIKE '%$search%' ORDER BY id asc") or die("Error in query: $search_query." . mysqli_error($cxn));
-
-                                if ($search_query) {
-                                    if (mysqli_num_rows($search_query) > 0) {
-                                        while ($r = mysqli_fetch_assoc($search_query)) {
-                                            echo '
-                                    <tbody>
-                                        <tr>
-                                            <td>' . $r['lrn_id'] . '</td>
-                                            <td>' . $r['first_name'] . '</td>
-                                            <td>' . $r['last_name'] . '</td>
-                                            <td>' . $r['grade_level'] . '</td>
-                                            <td>' . $r['address'] . '</td>
-                                            <td>' . $r['email'] . '</td>                            
-                                            <td> <a type="button" class="btn btn-primary btn-sm" href="searchData.php?id=' . $r['lrn_id'] . '" target="_blank">View More Info</a></td>                            
-                                        </tr>
-                                    </tbody>
-                                ';
-                                        }
-                                    } else {
-                                        echo '
-                                    <tbody>
-                                        <tr>
-                                            <td>
-                                                <h2>Data not found!</h2>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                    ';
-                                    }
-                                }
-                            }
+                        }
 
                             ?>
+                            <hr />
+                            <h3 class="card-title mb-3">Payment Records</h3>
+                            <table class="table table-hover table-borderless my-0" id="dataTable">
+                                <thead>
+                                    <tr>
+                                        <th>Reference No.</th>
+                                        <th>Payment Method</th>
+                                        <th>Payment Date</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    $getStudentPaymentData = mysqli_query($cxn, "SELECT * FROM payments WHERE lrn='$data'") or die("Error in query: $getStudentPaymentData." . mysqli_error($cxn));
+                                    $query = mysqli_num_rows($getStudentPaymentData);
+
+                                    if ($query > 0) {
+                                        $i = 0;
+                                        while ($p = mysqli_fetch_assoc($getStudentPaymentData)) {
+                                            $ref_no = $p['ref_no'];
+                                            $payment_method = $p['payment_method'];
+                                            $date = strtotime($p['uploaded_on']);
+                                            $pay_date = date("F d, Y; h:i A", $date);
+
+                                    ?>
+                                            <tr>
+                                                <td><?php echo $ref_no; ?></td>
+                                                <td><?php echo $payment_method; ?></td>
+                                                <td><?php echo $pay_date; ?></td>
+                                            </tr>
+
+                                    <?php
+                                            $i++;
+                                        }
+                                    } else {
+                                        echo "<tr>
+                          <td colspan='3'>No record found.</td>
+                      </tr>";
+                                    };
+
+                                    ?>
+                                </tbody>
+                            </table>
 
 
-                        </table>
+                            <br>
+                            <a href="javascript:window.print()" class="btn btn-primary btn-sm"><i class="fa fa-print" aria-hidden="true"></i> Print</a>
+                            <a href="javascript:window.close()" class="btn btn-secondary btn-sm"><i class="fa fa-reply" aria-hidden="true"></i> Back to Search</a>
+                            </div>
+                        </div>
                     </div>
-                </div>
             </div>
             <footer class="bg-white sticky-footer">
                 <div class="container my-auto">
