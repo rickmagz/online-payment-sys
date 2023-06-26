0fe199dc7ab7ceed3dd51f6d0f0f101b30aa60f6
Rick Allen Magbanua <ramnashor2@gmail.com>
2023-06-26 10:27:18 +0800

    Cloned from admin: Accept payment

--- treas/payment_accept.php
+++ treas/payment_accept.php
@@ -3,7 +3,9 @@ include 'db_connect.php';
 
 if (isset($_REQUEST['id'])) {
     $id = $_REQUEST['id'];
-    $accept_payment = mysqli_query($cxn, "UPDATE payments SET remarks='ACCEPTED' WHERE id='$id'");
+    $time = date_default_timezone_set('Asia/Manila');
+    $datestamp = date('Y-m-d H:i:s', time());
+    $accept_payment = mysqli_query($cxn, "UPDATE payments SET remarks='ACCEPTED', uploaded_on='$datestamp'  WHERE id='$id'");
 
     echo "<script type='text/javascript'> alert('Payment Accepted!'); location.href = 'payments.php'; </script>";
 }
