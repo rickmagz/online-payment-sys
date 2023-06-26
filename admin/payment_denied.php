c5a29525ef59504f4fd3c1d6036897091278489f
Rick Allen Magbanua <ramnashor2@gmail.com>
2023-06-26 10:29:17 +0800

    Added date function in deny payment

--- admin/payment_denied.php
+++ admin/payment_denied.php
@@ -3,7 +3,9 @@ include 'db_connect.php';
 
 if (isset($_REQUEST['id'])) {
     $id = $_REQUEST['id'];
-    $deny_payment = mysqli_query($cxn, "UPDATE payments SET remarks='DENIED' WHERE id='$id'");
+    $time = date_default_timezone_set('Asia/Manila');
+    $datestamp = date('Y-m-d H:i:s', time());
+    $deny_payment = mysqli_query($cxn, "UPDATE payments SET remarks='DENIED', uploaded_on='$datestamp'  WHERE id='$id'");
 
     echo "<script type='text/javascript'> alert('Payment DENIED!'); location.href = 'payments.php'; </script>";
 }
